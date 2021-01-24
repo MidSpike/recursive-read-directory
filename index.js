49cd21f419d0ea9ff0ac69e52d646523f317e02a
MidSpike <developer@midspike.com>
2021-01-24 10:03:37 -0500

    fixes

--- index.js
+++ index.js
@@ -4,31 +4,37 @@ const fs = require('fs');
 const path = require('path');
 
 /**
- * recursively goes through a directory to find files
- * @param {String} directory_path 
- * @returns {Array<String>} relative file paths inside of the `directory_path`
+ * Used to filter directories / files
+ * @param {String} directory_item a file or directory
  */
-module.exports = (directory_path) => {
+const item_filter_default = (directory_item) => !['node_modules', '.git', '.gitignore', '.env'].includes(directory_item);
+
+/**
+ * Recursively goes through a directory to find files
+ * @param {String} directory_path 
+ * @param {Function} item_filter a filter used to include/exclude directories/files
+ * @returns {Array<String>} relative file paths inside of the `directory_path`
+ * 
+ * @example
+ * ```js
+ * const item_filter = (directory_item) => !['node_modules', '.git', '.gitignore', '.env'].includes(directory_item)
+ * recursiveReadDirectory(process.cwd(), item_filter);
+ * ```
+ */
+module.exports = (directory_path, item_filter=item_filter_default) => {
     const found_files = [];
 
     function recursiveReadDirectory(recursive_directory_path) {
-        console.log('test 1', { recursive_directory_path });
-
         if (fs.existsSync(recursive_directory_path)) {
-            console.log('test 2 A');
             if (fs.statSync(recursive_directory_path).isDirectory()) {
-                console.log('test 3 A');
-                for (const filtered_directory_item of fs.readdirSync(recursive_directory_path).filter(item => item[0] !== '.')) {
-                    console.log('test 4', { filtered_directory_item });
+                for (const filtered_directory_item of fs.readdirSync(recursive_directory_path).filter(item_filter)) {
                     recursiveReadDirectory(path.join(recursive_directory_path, filtered_directory_item));
                 }
             } else {
-                console.log('test 3 B', { recursive_directory_path });
-                found_files.push(recursive_directory_path);
+                found_files.push(path.relative(directory_path, recursive_directory_path));
             }
         }
 
-        console.log('test 2 B');
         return found_files;
     }
 
